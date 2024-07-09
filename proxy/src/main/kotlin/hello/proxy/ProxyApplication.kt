a6019708c988b05b008ccca1f07a61e1ef0fc3a4
yuminee <ayuminee2@gmail.com>
2024-07-10 00:54:07 +0900

    파일 정리

---
+++ proxy/src/main/kotlin/hello/proxy/ProxyApplication.kt
@@ -0,0 +1,11 @@
+package hello.proxy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProxyApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProxyApplication>(*args)
+}
